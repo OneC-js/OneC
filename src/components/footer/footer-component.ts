4e5fdb3488820a8797a6d27e81d59a540151909f
nigel <nkmatyukira@outlook.com>
2021-06-04 17:24:42 +0200

    added footer content. replaced grid-gap with gap.

--- src/components/footer/footer-component.ts
+++ src/components/footer/footer-component.ts
@@ -5,7 +5,29 @@ export class FooterComponent extends OneComponent {
   // component definition
   $style = style;
   $template = {
-
+    div: {
+      _class: "container",
+      _cn: [
+        {
+          div: {
+            _class: "npm-text",
+            _text: "npm:"
+          }
+        },
+        {
+          div: {
+            _class: "url-container",
+            _text: "https://cdn.jsdelivr.net/npm/onecjs@1"
+          }
+        },
+        {
+          div: {
+            _class: "copy-button",
+            _text: "COPY",
+          },
+        },
+      ]
+    }
   };
 
   constructor() {
